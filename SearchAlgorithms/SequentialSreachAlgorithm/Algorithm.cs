facf2a05ee2947ee0e82e4b486db8f29ffa8189e
Andrius <andriusbogda1@gmail.com>
2021-12-04 17:17:19 +0200

    Created Algorithm.cs

---
+++ SearchAlgorithms/SequentialSreachAlgorithm/Algorithm.cs
@@ -0,0 +1,22 @@
+﻿namespace ads_sd3_12var.SearchAlgorithms
+{
+ internal class Algorithm
+ {
+  private string[] _stringArray;
+
+  public Algorithm(string[] parStringArray)
+  {
+   _stringArray = parStringArray;
+  }
+
+  public int SearchAndReturnIndex(string lookFor)
+  {
+   for (int i = 0; i < _stringArray.Length; i++)
+    if (_stringArray[0] == lookFor)
+    {
+     return i;
+    }
+   return -1;
+  }
+ }
+}
