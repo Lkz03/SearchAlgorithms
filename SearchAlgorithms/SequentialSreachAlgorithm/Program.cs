1704635ddf3e360f73fd03f75d8db2d5f8e50a32
Lkz03 <andriuspr21@gmail.com>
2021-12-09 07:45:39 +0200

    new test array

--- SearchAlgorithms/SequentialSreachAlgorithm/Program.cs
+++ SearchAlgorithms/SequentialSreachAlgorithm/Program.cs
@@ -2,7 +2,7 @@
 {
  public static void Main()
  {
-  int[] test = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  int[] test = new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
   var testClass = new Algorithm(test);
   var output = new OutputManager();
 
